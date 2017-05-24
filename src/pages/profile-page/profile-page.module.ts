bd4af0e067718fab8ba10ac556b0a123ad72ea96
Marni Brewster <marnibrewster@Kevins-iMac-3.fios-router.home>
2017-05-24 12:31:19 -0400

    updating profile view page

--- src/pages/profile-page/profile-page.module.ts
+++ src/pages/profile-page/profile-page.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfilePage } from './profile-page';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
