bd656e72eb5d2baac9dc06981dfa5e92a078bffc
Marni Brewster <marnibrewster@Kevins-iMac-3.fios-router.home>
2017-05-24 13:54:19 -0400

    adding search user capabilities

--- src/pages/inbox-page/inbox-page.ts
+++ src/pages/inbox-page/inbox-page.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MESSAGE_LIST } from '../../mocks/messages/messages';
 import { Message } from '../../models/messages/message.interface';
-/**
- * Generated class for the InboxPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 @IonicPage()
 @Component({
   selector: 'page-inbox-page',
@@ -23,5 +18,7 @@ export class InboxPage {
   ionViewDidLoad() {
     console.log(this.messageList);
   }
-
+  navigateToSearchUserPage() {
+    this.navCtrl.push('SearchUserPage');
+  }
 }
